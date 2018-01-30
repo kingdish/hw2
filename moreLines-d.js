e56ce09120bf720c88d53683acae4f896581aebe
kingdish <31574274+kingdish@users.noreply.github.com>
2018-01-29 19:45:18 -0800

    Update moreLines-d.js

--- moreLines-d.js
+++ moreLines-d.js
@@ -1,14 +1,19 @@
-function setup() {
-  createCanvas(340, 340);
-}
+background(255);
+colorMode(HSB);
+noStroke();
 
-function draw() {
-  startX = random(width);
-  startY = random(height);
-  endX = random(width);
-  endY = random(height);
-  length = Math.sqrt(Math.pow((endY - endX), 2) + Math.pow((startY - startX), 2));
-  colorMode(HSB);
-  stroke(length, length, length);
-  line(startX, startY, endX, endY);
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(random(60), 200 + random(55), 150+random(50));
+  shapeWidth = random(20, 50);
+  shapeHeight = random(20, 50);
+  startX = random(width) - shapeWidth;
+  startY = random(height) - shapeHeight;
+  beginShape();
+  	vertex(startX, startY);
+  	vertex(startX + shapeWidth, startY);
+ 	  vertex(startX, startY + shapeHeight);
+  	vertex(startX + shapeWidth, startY + shapeHeight);
+  endShape(CLOSE);
+  //ellipse(random(width), random(height), random(20, 40));
 }
