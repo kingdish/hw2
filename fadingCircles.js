ce5b1e3509a6a5fa4e41977817627e391a5ac37b
kingdish <31574274+kingdish@users.noreply.github.com>
2018-01-30 00:03:15 -0800

    Update fadingCircles.js

--- fadingCircles.js
+++ fadingCircles.js
@@ -4,6 +4,6 @@ noStroke();
 
 while(true) {
   background(0, 0, 255, 0.1);
-  fill(random(60), 200 + random(55), 150+random(50));
+  fill(random(40), 75 + random(25), 80+random(20));
   ellipse(random(width), random(height), random(20, 40));
 }
